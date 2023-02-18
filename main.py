7a5e062878342a255499c6804df70eb9e6308aea
Cacufa <carlilloscr@gmail.com>
2023-02-18 16:01:30 -0400

    started the unittest with flask

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 
 app =  Flask(__name__)
@@ -16,8 +17,11 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de Usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
-    
-    
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404) # error handler 404
 def not_found(error):
